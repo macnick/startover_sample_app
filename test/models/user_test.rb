76563ee55a5578d574db9d7512a7b29eff7da63e
macnick <nick@macexperts.gr>
2019-11-10 20:03:55 +0200

    Finish validations

---
+++ test/models/user_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup 
+    @user = User.new(name: 'Mac Nick', email: "mac@nick.com")
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'user name should be present' do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+end
