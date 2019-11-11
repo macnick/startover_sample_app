beb7bf6d25d0635b460ab53131264e9b29ca4f2d
macnick <nick@macexperts.gr>
2019-11-11 11:43:56 +0200

    Add avatar

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get '/signup', to: 'users#new'
-
+  resources :users
   # root 'application#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
