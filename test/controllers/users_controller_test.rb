3c95ed43914071e5c3468279cd001dd1db2b40d8
macnick <nick@macexperts.gr>
2019-11-12 19:28:07 +0200

    Finish Users index

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+  
   test "should get new" do
     get signup_path
     assert_response :success
