c9eb9233d8206a55943cc2b7124afbd716065aa2
macnick <nick@macexperts.gr>
2019-11-12 18:48:03 +0200

    Debug 10.2.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
       cookies.permanent[:remember_token] = user.remember_token
     end
 
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+      user == current_user
+    end
+    
     # Forgets a persistent session.
     def forget(user)
       user.forget
