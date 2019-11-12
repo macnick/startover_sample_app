df9bdec89e471a4d3440c2ec26a501a955668fa7
macnick <nick@macexperts.gr>
2019-11-12 12:38:21 +0200

    Implement basic login

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,7 +7,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path, count: 2
+    assert_select "a[href=?]", contact_path, count: 1
     assert_select "a[href=?]", signup_path
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
   
 end
