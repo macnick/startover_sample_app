24a2c7fcc4e3dd7c5404adb29605b1c4cc0c2c27
macnick <nick@macexperts.gr>
2019-11-22 13:17:41 +0200

    Finish 13.3.4

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -12,6 +13,9 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
@@ -20,4 +24,8 @@ class MicropostsController < ApplicationController
       params.require(:micropost).permit(:content)
     end
     
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
 end
